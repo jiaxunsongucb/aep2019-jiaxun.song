7b96732476c58c4e00f0079949f85c9d55a2eb9c
Jiaxun Song <jiaxun.song@outlook.com>
2019-04-24 16:04:35 -0700

    test_one_plus_two_should_equal_to_three

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,7 @@ cal = Calculator()
 
 def test_one_number_should_equal_to_itself():
     assert cal.calculate("1") == 1
+
+
+def test_one_plus_two_should_equal_to_three():
+    assert cal.calculate("1+2") == 3
