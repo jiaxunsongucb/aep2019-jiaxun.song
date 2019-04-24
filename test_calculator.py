6a01a23a15b264dd6fe7536f5c245cc5961071d3
Jiaxun Song <jiaxun.song@outlook.com>
2019-04-24 16:15:05 -0700

    test_one_plus_two_with_parentheses_should_equal_to_three

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,6 @@ def test_one_number_should_equal_to_itself():
 
 def test_one_plus_two_should_equal_to_three():
     assert cal.calculate("1+2") == 3
+
+def test_one_plus_two_with_parentheses_should_equal_to_three():
+    assert cal.calculate("(1+2)") == 3
